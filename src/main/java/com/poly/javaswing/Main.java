08c119c21977b7d1e7ac1e4e811ab557ff566ca0
Nguyen Trong Nghia (FPL CT) <nghiant96@fpt.edu.vn>
2025-06-06 18:14:16 +0700

    commit 2025.06.06

--- src/main/java/com/poly/javaswing/Main.java
+++ src/main/java/com/poly/javaswing/Main.java
@@ -9,8 +9,25 @@ package com.poly.javaswing;
  * @author macbookprom1
  */
 public class Main {
+    final String PRODUCT_NAME = "abc";
+    
     public static void main(String[] args){
-        MainFrame mainFrame = new MainFrame();
-        mainFrame.setVisible(true);
+//        MainFrame mainFrame = new MainFrame();
+//        mainFrame.setVisible(true);
+
+        AddProductFrame addProductFrame = new AddProductFrame();
+        addProductFrame.setVisible(true);
     }
 }
+
+// Khai báo hoặc tạo tên class hoặc 1 file .java hoa
+// => Chữ đầu tiên của mỗi từ phải viết hoa
+
+// Khai báo tên biến và tên phương thức 
+// Nếu trường hợp có 1 từ => tất cả ký tự viết thường 
+// Nếu có từ 2 từ trở lên => từ đầu tiên viết thường bất đầu từ từ thứ 2
+// trở đi viết hoa chữ cái đầu tiên 
+
+// Biến hằng số => là biến bắt buộc có giá trị và giá trị không thể thay đổi
+// Khai báo biến hằng số => Tất cả các ký tự phải viết hoa
+// Nếu có từ 2 từ trở lên thì các từ cách nhau bởi dấu _
